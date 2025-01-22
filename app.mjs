b90aa66ed407bcfa24a66f7390a291f30b7a524b
Cristian Barac Fabregat Gallegos <calmestend@gmail.com>
2025-01-22 00:13:31 -0600

    hotfix

--- app.mjs
+++ app.mjs
@@ -4,7 +4,24 @@ import { noticias } from './lib/instagram.mjs';
 const app = express()
 const port = 3000
 
+const scrapeInstagram = async(req, res) => {
+  const response = await fetch("https://scraper-api.smartproxy.com/v2/scrape", {
+    method: "POST",
+    body: JSON.stringify({
+      "target": "instagram_graphql_profile",
+      "query": "transportemx"
+    }),
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": "Basic UzAwMDAyMzI3MzE6UFdfMTA2YWYzNTllOTgwNzFhNTJhMjdlNzllMDdhMjEyZmFh"
+    },
+  }).catch(error => console.log(error));
+
+  res.json(await response.json())
+}
+
 app.get('/noticias', noticias); 
+app.get('/noticias/v2', scrapeInstagram);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
